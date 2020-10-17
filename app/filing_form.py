034cfa89eb8cb114514acd037c018de8295d4b40
Haley Morgan <haleymorgan3264@gmail.com>
2020-10-16 22:56:07 -0400

    create forms

---
+++ app/filing_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class FilingReqForm(FlaskForm):
+  ticker = StringField('ticker', validators=[DataRequired()])
+  filing_type = SelectField('filing', choices=[('10-K', '10-K'), ('10-Q'), ('10-Q')])
+  submit = SubmitField('Search')
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template
 from app import app
+from app.filing_form import FilingReqForm
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 @app.route('/index')
 def index():
-  return "Hello world"
+  form = FilingReqForm()
+  if form.validate_on_submit():
+    results = filing_request(form)
+    return render_template('index.html', form=form, data=results)
+  return render_template('index.html', form=form, data=None)
